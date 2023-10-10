6aaa214de59cc029979af6304c6e58fbebad2524
Yamkela <ncby9zfs7@vossie.net>
2023-10-10 16:26:05 +0200

    CORS

--- server/index.js
+++ server/index.js
@@ -14,9 +14,12 @@ app.use(express.json());
 app.use(cookieParser());
 
 // Add CORS middleware to allow requests from 'https://youtube-real-estate-xk5e.vercel.app'
-app.use(cors({
-    origin: 'https://youtube-real-estate-xk5e.vercel.app'
-}));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 
 app.use("/api/user", userRoute);
 app.use("/api/residency", residencyRoute);

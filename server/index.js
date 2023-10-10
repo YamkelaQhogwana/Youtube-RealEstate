c9eb7747b4077dfac866d45e79fa8c1c5ef59427
Yamkela <ncby9zfs7@vossie.net>
2023-10-10 15:34:17 +0200

    CORS Changes

--- server/index.js
+++ server/index.js
@@ -4,19 +4,23 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import { userRoute } from "./routes/userRoute.js";
 import { residencyRoute } from "./routes/residencyRoute.js";
+
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
 
+// Add CORS middleware to allow requests from 'https://youtube-real-estate-xk5e.vercel.app'
+app.use(cors({
+    origin: 'https://youtube-real-estate-xk5e.vercel.app'
+}));
 
-app.use("/api/user", userRoute)
-app.use("/api/residency", residencyRoute)
+app.use("/api/user", userRoute);
+app.use("/api/residency", residencyRoute);
 
-app.listen(port, ()=>{
-    console.log("The server is running on the port " + port )   
-})
+app.listen(port, () => {
+    console.log("The server is running on the port " + port );
+});

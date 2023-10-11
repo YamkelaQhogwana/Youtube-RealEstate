87e90798de7c76bc1665bebd904e1e88653fd8a4
Yamkela <ncby9zfs7@vossie.net>
2023-10-11 12:06:58 +0200

    NVM

--- server/config/auth0Config.js
+++ server/config/auth0Config.js
@@ -1,7 +1,7 @@
 import {auth} from 'express-oauth2-jwt-bearer'
 
 const jwtCheck = auth({
-    audience: "https://youtube-real-estate.vercel.app/api",
+    audience: "http://localhost:8000",
     issuerBaseURL: "https://dev-7t3kl4hb6o875wkm.us.auth0.com",
     tokenSigningAlg: "RS256"
 })
